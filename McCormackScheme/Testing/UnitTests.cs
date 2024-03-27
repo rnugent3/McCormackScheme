cf6cf55f82eb83fcfa4666417e9ba9a9f19f0555
rnugent3 <richard.nugent.3@gmail.com>
2024-03-26 20:02:55 -0500

    binaries cleaned out and confirmed local things still work

--- McCormackScheme/Testing/UnitTests.cs
+++ McCormackScheme/Testing/UnitTests.cs
@@ -2,6 +2,12 @@ namespace Testing
 {
     public class UnitTests
     {
+        /// <summary>
+        /// This is the typical format of a unit test
+        /// </summary>
+        /// <param name="depth"></param>
+        /// <param name="bankSlope"></param>
+        /// <param name="expected"></param>
         [Theory]
         [InlineData(10, 1.5, 160)]
         public void HydraulicRadius(double depth, double bankSlope, double expected)
